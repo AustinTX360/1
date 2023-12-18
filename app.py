c77806bb007200c079ebb5b1ceb07028fc16ed45
Jian Xu <153311920+AustinTX360@users.noreply.github.com>
2023-12-18 14:31:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,15 @@ def submit_form():
     
     return f'Thank you, {name}! Your email ({email}) regarding "{subject}" has been submitted.'
 
+@app.route('/submit_contact_form', methods=['POST'])
+def submit_contact_form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    
+    # Process the contact form data (you can add your own logic here)
+    
+    return f'Thank you, {name}! Your email ({email}) with the message "{message}" has been submitted.'
+
 if __name__ == '__main__':
     app.run(debug=True)

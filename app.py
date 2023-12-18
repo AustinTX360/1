aea7bcc5793eaba633f2171109cfa9e6875750eb
Jian Xu <153311920+AustinTX360@users.noreply.github.com>
2023-12-18 11:03:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    
+    # Process the form data (you can add your own logic here)
+    
+    return f'Thank you, {name}! Your email ({email}) has been submitted.'
 
 if __name__ == '__main__':
     app.run(debug=True)

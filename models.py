18309dd41f5a38881fc61c6acf9b6e65b129358f
Jian Xu <153311920+AustinTX360@users.noreply.github.com>
2023-12-19 14:33:00 -0600

    Update models.py

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ db = SQLAlchemy()
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    address = db.Column(db.String(255))
-    energy_usage = db.Column(db.Float)
-    num_cars = db.Column(db.Integer)
-    energy_storage = db.Column(db.Float)
+    name = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.Text, nullable=False)
+    energy_usage = db.Column(db.Float, nullable=False)
+    num_cars = db.Column(db.Integer, nullable=False)
+    energy_storage = db.Column(db.Float, nullable=False)

0c313b36571471a5a71b3f53a1ea9746e04a6c0d
Jian Xu <153311920+AustinTX360@users.noreply.github.com>
2023-12-19 14:27:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,14 +31,6 @@ db = SQLAlchemy(app)
 all_visitors = []
 question_submissions = []
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    address = db.Column(db.Text, nullable=False)
-    energy_usage = db.Column(db.Float, nullable=False)
-    num_cars = db.Column(db.Integer, nullable=False)
-    energy_storage = db.Column(db.Float, nullable=False)
-
 # Function to check if the user is logged in as an admin
 def is_admin():
     return session.get('username') in admins

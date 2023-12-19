ec9c368f7463532e5f6deaf6267c901f60ff4533
Jian Xu <153311920+AustinTX360@users.noreply.github.com>
2023-12-19 15:27:16 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -9,3 +10,10 @@ class Product(db.Model):
     energy_usage = db.Column(db.Float, nullable=False)
     num_cars = db.Column(db.Integer, nullable=False)
     energy_storage = db.Column(db.Float, nullable=False)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(50), nullable=False)

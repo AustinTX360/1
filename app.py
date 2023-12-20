5891c3cb8c02964570eca03578aa464f4bbecd6a
Jian Xu <153311920+AustinTX360@users.noreply.github.com>
2023-12-19 20:05:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ import logging
 import sys
 from models import db, Product, User
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from applicationinsights.flask.ext import AppInsights
 
 app = Flask(__name__)
 # Add the following lines to configure logging to print debug messages
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.DEBUG)
+appinsights = AppInsights(app)
 
+# force flushing application insights handler after each request
+@app.after_request
+def after_request(response):
+    appinsights.flush()
+    return response
+    
 app.secret_key = 'your_secret_key'  # Change this to a secure, random key in a production environment
 admins = {'admin': 'admin_password'}  # Add your admin credentials here
 #app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://jz20000cn:'820916Yg!'@nodaldata.database.windows.net/Nodal?driver=ODBC+Driver+18+for+SQL+Server"
